022ac2e560141d3863fbe23a0c554ecac92bb0d9
mikezangus <zangusm@gmail.com>
2023-10-16 21:40:25 -0400

    user choice

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,23 +5,20 @@ import requests
 import time
 
 base_directory = "/Users/zangus/Documents/Projects/Project_CREAM"
-
 data_directory = os.path.join(base_directory, "data")
-_2022_PA_17_directory = os.path.join(data_directory, "2022", "PA", "17")
-clean_directory = os.path.join(data_directory, "clean")
-file = input("\nType '1' to clean Deluzio, type '2' to clean DeMarco, type '3' to clean Shaffer: ")
-if file == "1":
-    input_filename = "2022_PA_17_DELUZIO_CHRISTOPHER_DEMOCRATIC_source.csv"
-elif file == "2":
-    input_filename = "2022_PA_17_DEMARCO_SAMUEL-III_REPUBLICAN_source.csv"
-elif file == "3":
-    input_filename = "2022_PA_17_SHAFFER_JEREMY_REPUBLICAN_source.csv"
+source_data_directory = os.path.join(data_directory, "source")
+clean_data_directory = os.path.join(data_directory, "clean")
 
-input_path = os.path.join(_2022_PA_17_directory, input_filename)
+try:
+    with open(os.path.join(base_directory, "config.json"), "r") as file:
+        config = json.load(file)
+        api_key = config["googleMapsApiKey"]
+    if not api_key:
+        raise ValueError("Google Maps API Key is missing from the config file")
+except FileNotFoundError:
+    print("Config file not found")
+    exit()
 
-with open(os.path.join(base_directory, "config.json"), "r") as file:
-    config = json.load(file)
-api_key = config["googleMapsApiKey"]
 
 def get_coordinates(address, api_key):
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -38,75 +35,111 @@ def get_coordinates(address, api_key):
             return [latitude, longitude]
     return None
 
-candidate_info = input_filename.split("_")
-year, state, district, last_name, first_name, party = candidate_info[:6]
 
-relevant_columns = [
-    "transaction_id",
-    "entity_type_desc",
-    "contributor_street_1",
-    "contributor_city",
-    "contributor_state",
-    "contributor_zip",
-    "contribution_receipt_date",
-    "contribution_receipt_amount",
-    "candidate_name",
-    "candidate_office_full",
-    "candidate_office_state",
-    "candidate_office_district",
-    "donor_committee_name",
-    "fec_election_type_desc",
-    "fec_election_year"
-]
+def clean_data(filename, input_path):
+    candidate_info = filename.split("_")
+    year, state, district, last_name, first_name, party = candidate_info[:6]
+    print(f"Starting to clean {year} {state}-{district} candidate {first_name} {last_name}'s data")
 
-data = pd.read_csv(filepath_or_buffer = input_path, sep = ",", usecols = relevant_columns)
+    relevant_columns = [
+        "transaction_id",
+        "entity_type_desc",
+        "contributor_street_1",
+        "contributor_city",
+        "contributor_state",
+        "contributor_zip",
+        "contribution_receipt_date",
+        "contribution_receipt_amount",
+        "candidate_name",
+        "candidate_office_full",
+        "candidate_office_state",
+        "candidate_office_district",
+        "donor_committee_name",
+        "fec_election_type_desc",
+        "fec_election_year"
+    ]
 
-data["candidate_last_name"] = last_name
-data["candidate_first_name"] = first_name
-data["candidate_state"] = state
-data["candidate_district"] = str(district)
-data["candidate_party"] = party
+    data = pd.read_csv(filepath_or_buffer = input_path, sep = ",", usecols = relevant_columns)
 
-data["transaction_id"] = data["transaction_id"].astype(str)
-data["contributor_zip"] = data["contributor_zip"].astype(str)
-data["candidate_office_district"] = data["candidate_office_district"].apply(lambda x: str(int(x)) if not pd.isna(x) else x)
-data["fec_election_year"] = data["fec_election_year"].fillna(0).astype(int).astype(str)
-data = data.query("fec_election_year == @year")
-data["contribution_receipt_amount"] = data["contribution_receipt_amount"].astype(float)
-data["contribution_receipt_date"] = pd.to_datetime(data["contribution_receipt_date"], errors = "coerce")
-data["contribution_receipt_date"] = data["contribution_receipt_date"].dt.strftime("%Y-%m-%d")
+    data["candidate_last_name"] = last_name
+    data["candidate_first_name"] = first_name
+    data["candidate_state"] = state
+    data["candidate_district"] = str(district)
+    data["candidate_party"] = party
 
-data["full_address"] = data["contributor_street_1"] + ", " + data["contributor_city"] + ", " + data["contributor_state"] + " " + data["contributor_zip"]
-address_count = len(data["full_address"])
-start_time = time.time()
-print(f"Converting {address_count} addresses to coordinates, starting at {time.strftime('%H:%M:%S', time.localtime(start_time))}...")
-data["contributor_location"] = data.apply(lambda row: get_coordinates(row["full_address"], api_key), axis = 1)
-end_time = time.time()
-total_time = round((end_time - start_time) / 60, 4)
-print(f"Finished converting {address_count} addresses to coordinates in {total_time} minutes at a rate of {(address_count / total_time):.2f} addresses per minute")
-data.drop(columns = ["contributor_street_1", "contributor_city", "contributor_state", "contributor_zip", "full_address"], inplace = True)
+    data["transaction_id"] = data["transaction_id"].astype(str)
+    data["contributor_zip"] = data["contributor_zip"].astype(str)
+    data["candidate_office_district"] = data["candidate_office_district"].apply(lambda x: str(int(x)) if not pd.isna(x) else x)
+    data["fec_election_year"] = data["fec_election_year"].fillna(0).astype(int).astype(str)
+    data = data.loc[data["fec_election_year"] == year]
+    data["contribution_receipt_amount"] = data["contribution_receipt_amount"].astype(float)
+    data["contribution_receipt_date"] = pd.to_datetime(data["contribution_receipt_date"], errors = "coerce")
+    data["contribution_receipt_date"] = data["contribution_receipt_date"].dt.strftime("%Y-%m-%d")
 
-column_order = [
-    "transaction_id",
-    "candidate_last_name",
-    "candidate_first_name",
-    "fec_election_year",
-    "candidate_state",
-    "candidate_district",
-    "candidate_party",
-    "contribution_receipt_date",
-    "contribution_receipt_amount",
-    "entity_type_desc",
-    "contributor_location",
-    "fec_election_type_desc",
-    "donor_committee_name",
-    "candidate_office_full",
-    "candidate_office_state",
-    "candidate_office_district"
-]
+    data["full_address"] = data["contributor_street_1"] + ", " + data["contributor_city"] + ", " + data["contributor_state"] + " " + data["contributor_zip"]
+    address_count = len(data["full_address"])
+    start_time = time.time()
+    print(f"Converting {address_count} addresses to coordinates, starting at {time.strftime('%H:%M:%S', time.localtime(start_time))}...")
+    data["contributor_location"] = data.apply(lambda row: get_coordinates(row["full_address"], api_key), axis = 1)
+    end_time = time.time()
+    total_time = round((end_time - start_time) / 60, 4)
+    print(f"Finished converting {address_count} addresses to coordinates in {total_time:.2f} minutes at a rate of {(address_count / total_time):.2f} addresses per minute")
+    data.drop(columns = ["contributor_street_1", "contributor_city", "contributor_state", "contributor_zip", "full_address"], inplace = True)
 
-data = data.reindex(columns = column_order)
-output_filename = f"{year}_{state}_{district}_{last_name}_{first_name}_{party}_clean.csv"
-output_path = os.path.join(clean_directory, output_filename)
-data.to_csv(path_or_buf = output_path, index = False)
-print(f"Cleaned data saved to {output_path}")
+    column_order = [
+        "transaction_id",
+        "candidate_last_name",
+        "candidate_first_name",
+        "fec_election_year",
+        "candidate_state",
+        "candidate_district",
+        "candidate_party",
+        "contribution_receipt_date",
+        "contribution_receipt_amount",
+        "entity_type_desc",
+        "contributor_location",
+        "fec_election_type_desc",
+        "donor_committee_name",
+        "candidate_office_full",
+        "candidate_office_state",
+        "candidate_office_district"
+    ]
+
+    data = data.reindex(columns = column_order)
+    return data
+
+
+def save_cleaned_data(district_paths):
+    for district_path in district_paths:
+        for filename in os.listdir(district_path):
+            if filename.startswith("."): 
+                continue
+            input_path = os.path.join(district_path, filename)
+            clean_district_path = district_path.replace(source_data_directory, clean_data_directory)
+            output_filename = filename.replace("_source.csv", "_clean.csv")
+            output_path = os.path.join(clean_district_path, output_filename)
+            if os.path.exists(output_path):
+                continue
+            cleaned_data = clean_data(filename, input_path)
+            os.makedirs(clean_district_path, exist_ok = True)
+            cleaned_data.to_csv(path_or_buf = output_path, index = False)
+            print(f"Cleaned data saved to {output_path}")
+
+
+def process_district_data(year, state, district_choice):
+    if district_choice == "all":
+        all_districts = sorted([d for d in os.listdir(os.path.join(source_data_directory, str(year), state)) if os.path.isdir(os.path.join(source_data_directory, str(year), state, d))], key = lambda x: int(x) if x.isdigit() else 0)
+        for district in all_districts:
+            district_path = os.path.join(source_data_directory, str(year), state, district)
+            save_cleaned_data([district_path])
+    else:
+        district_path = os.path.join(source_data_directory, str(year), state, district_choice)
+        save_cleaned_data([district_path])
+
+
+if __name__ == "__main__":
+    year = "2022"
+    state = input("From which state do you want to clean data?: ").upper()
+    district_choice = input(f"From which {state} district do you want to clean data?\nTo process one district, enter the district's number, or to process all of {state}'s districts, enter 'all': ")
+    process_district_data(year, state, district_choice)
+    print("Data cleaning completed")
--- data_uploader.py
+++ data_uploader.py
@@ -7,42 +7,77 @@ base_directory = "/Users/zangus/Documents/Projects/Project_CREAM"
 
 data_directory = os.path.join(base_directory, "data")
 clean_directory = os.path.join(data_directory, "clean")
-file = input("\nType '1' to upload Deluzio, type '2' to upload DeMarco, type '3' to upload Shaffer: ")
-if file == "1":
-    upload_filename = "2022_PA_17_DELUZIO_CHRISTOPHER_DEMOCRATIC_clean.csv"
-elif file == "2":
-    upload_filename = "2022_PA_17_DEMARCO_SAMUEL-III_REPUBLICAN_clean.csv"
-elif file == "3":
-    upload_filename = "2022_PA_17_SHAFFER_JEREMY_REPUBLICAN_clean.csv"
-
-upload_path = os.path.join(clean_directory, upload_filename)
 
 with open(os.path.join(base_directory, "config.json"), "r") as file:
     config = json.load(file)
-
 config["uri"] = f"mongodb+srv://{config['mongoUsername']}:{config['mongoPassword']}@{config['mongoCluster']}.px0sapn.mongodb.net/{config['mongoDatabase']}?retryWrites=true&w=majority"
+
 client = MongoClient(config["uri"])
 db = client[config["mongoDatabase"]]
 
-data = pd.read_csv(upload_path, dtype = {
-    "transaction_id": str,
-    "candidate_district": str,
-    "fec_election_year": str,
-    "candidate_office_district": str
-})
-data["contribution_receipt_amount"] = data["contribution_receipt_amount"].astype(float)
-data["contributor_location"] = data["contributor_location"].apply(
-    lambda coord_string: [float(x) for x in coord_string[1:-1].split(", ")] if isinstance(coord_string, str) else coord_string
-)
+def get_user_choices():
+    year = get_user_choice("Which year's data do you want to upload?: ", os.listdir(clean_directory))
+    states = os.listdir(os.path.join(clean_directory, year))
+    state = get_user_choice("From which state do you want to upload data?: ", states)
+    all_districts = ["all"] + os.listdir(os.path.join(clean_directory, year, state))
+    district = get_user_choice(f"From which {state} district do you want to upload data?: ", all_districts)
+    district_path = os.path.join(clean_directory, year, state, district) if district != "all" else os.path.join(clean_directory, year, state)
+    candidate_files = [f for f in os.listdir(district_path) if f.endswith("_clean.csv")]
+    candidates = ["all"] + [f.split('_')[3] for f in candidate_files]
+    candidate = get_user_choice("Which candidate do you want to upload?: ", candidates)
+    return year, state, district, candidate
 
-collection_name = upload_filename[:7]
-collection = db[collection_name]
+def get_user_choice(prompt, available_options):
+    print("Available options:", available_options)
+    choice = input(prompt).strip()
+    while choice.upper() not in [option.upper() for option in available_options]:
+        print("Invalid choice. Please choose from the available options.")
+        choice = input(prompt).strip()
+    return choice.upper()
 
-records = data.to_dict(orient = "records")
-for record in records:
-    collection.update_one(
-        {"transaction_id": record["transaction_id"]},
-        {"$set": record},
-        upsert = True
+def process_upload(upload_path, db):
+    data = pd.read_csv(upload_path, dtype={
+        "transaction_id": str,
+        "candidate_district": str,
+        "fec_election_year": str,
+        "candidate_office_district": str
+    })
+    data["contribution_receipt_amount"] = data["contribution_receipt_amount"].astype(float)
+    data["contributor_location"] = data["contributor_location"].apply(
+        lambda coord_string: [float(x) for x in coord_string[1:-1].split(", ")] if isinstance(coord_string, str) else coord_string
     )
-print(f"Data uploaded to MongoDB collection: {collection_name}")
+    collection_name = os.path.basename(upload_path)[:7]
+    print(collection_name)
+    collection = db[collection_name]
+
+    records = data.to_dict(orient = "records")
+    for record in records:
+        collection.update_one(
+            {"transaction_id": record["transaction_id"]},
+            {"$set": record},
+            upsert = True
+        )
+
+year, state, district, candidate = get_user_choices()
+
+if district == "all":
+    districts = os.listdir(os.path.join(clean_directory, year, state))
+else:
+    districts = [district]
+
+for district in districts:
+    path = os.path.join(clean_directory, year, state, district)
+    print(f"Checking path: {path}")
+    all_files = os.listdir(path)
+    print(f"All files in directory: {all_files}")
+    if candidate == "ALL":
+        files = [f for f in all_files if f.endswith("_clean.csv")]
+    else:
+        files = [f for f in all_files if candidate in f.upper()]
+
+    print(f"Files to be processed: {files}")
+    for file in files:
+        print(f"Processing file {file}")
+        process_upload(os.path.join(path, file), db)
+
+print("Data uploading completed")
