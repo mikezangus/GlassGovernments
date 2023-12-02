60e217916556b47657a62c2702eba71159232089
mikezangus <zangusm@gmail.com>
2023-12-02 15:48:53 -0500

    creation of utilities file

---
+++ utilities.py
@@ -0,0 +1,8 @@
+import os
+
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(current_path, "data")
+downloads_container_dir = os.path.join(data_dir, "downloads_container")
+source_data_dir = os.path.join(data_dir, "source")
+cleaned_data_dir = os.path.join(data_dir, "cleanx")
