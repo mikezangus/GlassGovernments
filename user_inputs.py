a51015f1b1e906c1bd989a2f25ed834fac58b3df
mikezangus <zangusm@gmail.com>
2023-12-08 23:04:30 -0500

    more modular

--- user_inputs.py
+++ user_inputs.py
@@ -1,102 +1,130 @@
+import json
 import os
 
 
-def get_user_input(action, callback, data_dir):
+def get_user_inputs(action: str, chamber: bool = None, data_dir = None):
 
 
-    def is_valid_input(choice, options):
-        return choice in options or choice.lower() == "all"
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(current_dir, "us_states_all.json"), "r") as us_states_all_file:
+        us_states_all = json.load(us_states_all_file)
+    with open(os.path.join(current_dir, "us_states_at_large.json"), "r") as us_states_at_large_file:
+        us_states_at_large = json.load(us_states_at_large_file)
 
 
-    # 1. Year
-    def decide_year():
-        years = sorted([y for y in os.listdir(data_dir) if not y.startswith(".")])
-        while True:
-            year_input = input(str(f"From which year do you want to {action} data?:\n{', '.join(years)}\n> "))
-            if year_input in years:
-                decide_state(year = year_input)
-                break
-            else:
-                print("You've entered an invalid year, try again")
+    def input_choice(subject: str, action: str, choices: list = None):
+        choices_text = f"\n{', '.join(choices)}" if choices else ""
+        input_message = input(f"From which {subject} do you want to {action} data?:{choices_text}\n> ")
+        return input_message
+        
+
+    def print_retry_message(subject: str):
+        retry_message = print(f"You've entered an invalid {subject}, try again")
+        return retry_message
+        
+
+    def is_valid_input(choice: str, choices: list):
+        return choice in choices or choice.lower() == "all"
 
 
-    # 2. State
-    def decide_state(year):
-        states_dir = os.path.join(data_dir, year)
-        states = sorted([s for s in os.listdir(states_dir) if not s.startswith(".")])
-        while True:
-            state_input = input(f"From which state do you want to {action} files? For all states, enter 'all':\n{', '.join(states)}\n> ").upper()
-            if is_valid_input(choice = state_input, options = states):
-                if state_input.lower() == "all":
-                    process_all_states(year = year)
-                else:
-                    decide_district(year = year, state = state_input)
-                break
-            else:
-                print("You've entered an invalid state, try again")
-
-    def process_all_states(year):
-        print(f"\n{action.capitalize()}ing data from {year} for all states\n{'-' * 100}")
-        states_dir = os.path.join(data_dir, year)
-        states = sorted([s for s in os.listdir(states_dir) if not s.startswith(".")])
-        for state in states:
-            process_all_districts(year = year, state = state)
-
-
-    # 3. District
-    def decide_district(year, state):
-        districts_dir = os.path.join(data_dir, year, state)
-        districts = sorted([d for d in os.listdir(districts_dir) if not d.startswith(".")])
-        if len(districts) == 1:
-            decide_candidate(year = year, state = state, district = districts[0])   
-        else:
+    def decide_year(chamber: bool = None):
+        subject = "year"
+        if not chamber:
+            years = sorted([y for y in os.listdir(data_dir) if not y.startswith(".")])
             while True:
-                district_input = input(f"From which {state} district do you want to {action} files? For all districts, enter 'all':\n{', '.join(districts)}\n> ")
-                if is_valid_input(choice = district_input, options = districts):
-                    if district_input.lower() == "all":
-                        process_all_districts(year = year, state = state)
-                    else:
-                        decide_candidate(year = year, state = state, district = district_input)
-                    break
+                year_input = input_choice(subject = subject, action = action, choices = years)
+                if year_input in years:
+                    return year_input
                 else:
-                    print("You've entered an invalid district, try again")
+                    print_retry_message(subject = subject)
+                    continue
+        else:
+            year_input = input_choice(subject = subject, action = action)
+            return year_input
+            
 
-    def process_all_districts(year, state):
-        print(f"\n{action.capitalize()}ing data for all {state} districts\n{'-' * 100}")
-        districts_dir = os.path.join(data_dir, year, state)
-        districts = sorted([d for d in os.listdir(districts_dir) if not d.startswith(".")])
-        for district in districts:
-            process_all_candidates(year = year, state = state, district = district)
+    def decide_chamber():
+        subject = "chamber"
+        chambers = ["House", "Senate"]
+        while True:
+            chamber_input = input_choice(subject = subject, action = action, choices = chambers).capitalize()
+            if is_valid_input(choice = chamber_input, choices = chambers):
+                return chamber_input.lower()
+            else:
+                print_retry_message(subject = subject)
+                continue
 
 
-    # 4. Candidate
-    def decide_candidate(year, state, district):
+    def decide_state(year: str, chamber: str = None):
+        subject = "state"
+        if not chamber:
+            states_dir = os.path.join(data_dir, year)
+            states = sorted([s for s in os.listdir(states_dir) if not s.startswith(".")])
+            while True:
+                state_input = input_choice(subject = subject, action = action, choices = states).upper()
+                if is_valid_input(choice = state_input, choices = states):
+                    return state_input
+                else:
+                    print_retry_message(subject = subject)
+                    continue
+        else:
+            states = list(us_states_all.values())
+            while True:
+                state_input = input_choice(subject = "state", action = action, choices = states).upper()
+                if is_valid_input(choice = state_input, choices = states):
+                    return state_input
+                else:
+                    print_retry_message(subject = subject)
+                    continue
+
+
+    def decide_district(year: str, state: str, chamber: str = None):
+        subject = "district"
+        if not chamber:
+            districts_dir = os.path.join(data_dir, year, state)
+            districts = sorted([d for d in os.listdir(districts_dir) if not d.startswith(".")])
+            if len(districts) == 1:
+                district_input = districts[0]
+                return district_input
+            else:
+                while True:
+                    district_input = input_choice(subject = "district", action = action, choices = districts)
+                    if is_valid_input(choice = district_input, choices = districts):
+                        return district_input
+                    else:
+                        print_retry_message(subject = subject)
+                        continue
+        else:
+            district_input = input_choice(subject = "district", action = action)
+            return district_input
+
+
+    def decide_candidate(year: str, state: str, district: str):
+        subject = "candidate"
         candidates_dir = os.path.join(data_dir, year, state, district)
         source_file_names = sorted([f for f in os.listdir(candidates_dir) if not f.startswith(".") and f.endswith(".csv") and f.count("_") == 6])
         candidate_last_names = [file.split("_")[3] for file in source_file_names]
         while True:
-            candidate_input = input(f"Which {state}-{district} candidate's file do you want to {action}? For all candidates, enter 'all':\n{', '.join(candidate_last_names)}\n> ").upper()
-            if is_valid_input(choice = candidate_input, options = candidate_last_names):
-                if candidate_input.lower() == "all":
-                    process_all_candidates(year = year, state = state, district = district)
-                else:
-                    for source_file_name in source_file_names:
-                        if source_file_name.split("_")[3].upper() == candidate_input:
-                            process_one_candidate(year = year, state = state, district = district, file_name = source_file_name)  
-                break
+            candidate_input = input_choice(subject = "candidate", action = action, choices = candidate_last_names).upper()
+            if is_valid_input(choice = candidate_input, choices = candidate_last_names):
+                return candidate_input
             else:
-                print("You've entered an invalid candidate, try again")     
-    
-    def process_all_candidates(year, state, district):
-        print(f"\n{action.capitalize()}ing data for all {state}-{district} candidates\n{'-' * 75}")
-        candidates_dir = os.path.join(data_dir, year, state, district)
-        file_names = [f for f in os.listdir(candidates_dir) if not f.startswith(".") and f.count("_") == 6]
-        for file_name in file_names:
-            process_one_candidate(year = year, state = state, district = district, file_name = file_name)
-    
-    def process_one_candidate(year, state, district, file_name):
-        callback(year = year, state = state, district = district, file_name = file_name)
+                print_retry_message(subject = subject)  
+                continue
 
-
-    # 5. Run
-    decide_year()
+    
+    year = decide_year(chamber = chamber)
+    if chamber:
+        chamber = decide_chamber()
+    else:
+        chamber = None
+    state = decide_state(chamber = chamber, year = year)
+    if state in us_states_at_large.values():
+        district = "00"
+    else:
+        district = decide_district(chamber = chamber, year = year, state = state)
+    if chamber:
+        candidate = None
+    else:
+        candidate = decide_candidate(year = year, state = state, district = district)
+    return year, chamber, state, district, candidate
