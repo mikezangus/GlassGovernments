787add4a058325303ae6cd4c399a24e60bf2c5c0
mikezangus <zangusm@gmail.com>
2024-01-04 15:25:16 -0500

    covfefe

--- data_cleaning/driver.py
+++ data_cleaning/driver.py
@@ -5,8 +5,9 @@ from engine import clean_one_candidate
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(current_dir)
 sys.path.append(project_dir)
-from user_inputs.driver_user_inputs import get_user_inputs
+from caffeinate import start_caffeinate, stop_caffeinate
 from project_directories import load_data_dir, load_raw_data_dir
+from user_inputs.driver_user_inputs import get_user_inputs
 
 
 def main():
@@ -15,8 +16,12 @@ def main():
     raw_data_dir = load_raw_data_dir(data_dir)
 
     candidate_list = get_user_inputs("clean", "files", raw_data_dir)
-    for candidate in candidate_list:
-        clean_one_candidate(candidate)
+    caffeinate_process = start_caffeinate()
+    try:
+        for candidate in candidate_list:
+            clean_one_candidate(candidate)
+    finally:
+        stop_caffeinate(caffeinate_process)
 
 
 if __name__ == "__main__":
