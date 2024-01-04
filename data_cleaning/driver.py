191aea918296b028ba3463da8e8f85cfb47d845f
mikezangus <zangusm@gmail.com>
2024-01-03 23:35:54 -0500

    data cleaning now compatible with new user inputs list structure

--- data_cleaning/driver.py
+++ data_cleaning/driver.py
@@ -1,17 +1,22 @@
 import os
 import sys
-from workflows import determine_workflow
+from engine import clean_one_candidate
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(current_dir)
 sys.path.append(project_dir)
-from project_directories import raw_data_dir, cleaned_data_dir
 from user_inputs.driver_user_inputs import get_user_inputs
+from project_directories import load_data_dir, load_raw_data_dir
 
 
 def main():
-    year_list, chamber_list, state_list, district_list, candidate_list = get_user_inputs("files", "clean")
-    determine_workflow(year_list, chamber_list, state_list, district_list, candidate_list, raw_data_dir, cleaned_data_dir)
+
+    data_dir = load_data_dir(project_dir)
+    raw_data_dir = load_raw_data_dir(data_dir)
+
+    candidate_list = get_user_inputs("clean", "files", raw_data_dir)
+    for candidate in candidate_list:
+        clean_one_candidate(candidate)
 
 
 if __name__ == "__main__":
--- data_cleaning/engine.py
+++ data_cleaning/engine.py
@@ -1,16 +1,32 @@
+import os
+import sys
 from modules.raw_file_loader import load_raw_file
 from modules.candidate_info_injector import inject_candidate_info
 from modules.address_converter import convert_addresses_to_coordinates
 from modules.column_arranger import arrange_columns
 from modules.cleaned_file_saver import save_cleaned_file
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.dirname(current_dir)
+sys.path.append(project_dir)
+from project_directories import load_data_dir, load_raw_data_dir, load_cleaned_data_dir
+data_dir = load_data_dir(project_dir)
+raw_data_dir = load_raw_data_dir(data_dir)
+cleaned_data_dir = load_cleaned_data_dir(data_dir)
 
-def clean_one_candidate(year: str, chamber: str, state: str, candidate: str, raw_data_dir: str, cleaned_data_dir: str, district: str = None):
-    candidate_info = candidate.split("_")[:6]
-    _, _, _, last_name, first_name, party = candidate_info
+
+def clean_one_candidate(candidate: str):
+
+    split_index = [pos for pos, char in enumerate(candidate) if char == "_"][3]
+    constituency = candidate[:split_index]
+    raw_file_name = candidate[split_index + 1:]
+    year, chamber, state, district = constituency.split("_")
+    last_name, first_name, party = raw_file_name.split("_")[3:6]
+
     subject = f"{state}-{district} candidate {first_name} {last_name}"
-    data = load_raw_file(year, chamber, state, candidate, raw_data_dir, district)
-    data = inject_candidate_info(data, state, last_name, first_name, party, district)
+
+    data = load_raw_file(year, chamber, state, raw_file_name, raw_data_dir, district)
+    data = inject_candidate_info(data, state, district, last_name, first_name, party)
     data = convert_addresses_to_coordinates(data, subject)
     data = arrange_columns(data)
-    save_cleaned_file(data, year, chamber, state, candidate, cleaned_data_dir, district)
+    save_cleaned_file(data, year, chamber, state, raw_file_name, cleaned_data_dir, district)
