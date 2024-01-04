b4621d4d92b86d394d61b84a20c7137e66ba69c9
mikezangus <zangusm@gmail.com>
2024-01-03 23:30:42 -0500

    data uploading now compatible with new user inputs list structure

--- data_uploading/driver.py
+++ data_uploading/driver.py
@@ -1,17 +1,23 @@
 import os
 import sys
 
-from workflows import determine_workflow
+from engine import upload_one_candidate
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(current_dir)
 sys.path.append(project_dir)
 from user_inputs.driver_user_inputs import get_user_inputs
+from project_directories import load_data_dir, load_cleaned_data_dir
 
 
 def main():
-    year, chamber_list, state_list, district_list, candidate_list = get_user_inputs("files", "upload")
-    determine_workflow(year, chamber_list, state_list, district_list, candidate_list)
+
+    data_dir = load_data_dir(project_dir)
+    cleaned_data_dir = load_cleaned_data_dir(data_dir)
+
+    candidate_list = get_user_inputs("upload", "files", cleaned_data_dir)
+    for candidate in candidate_list:
+        upload_one_candidate(candidate, cleaned_data_dir)
 
 
 if __name__ == "__main__":
--- data_uploading/engine.py
+++ data_uploading/engine.py
@@ -6,16 +6,18 @@ from modules.cleaned_file_path_constructor import construct_cleaned_file_path
 from modules.data_type_converter import covert_data_types
 from modules.data_uploader import upload_data
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_dir = os.path.dirname(current_dir)
-sys.path.append(project_dir)
-from project_directories import cleaned_data_dir
 
+def upload_one_candidate(candidate: str, cleaned_data_dir: str):
 
-def upload_one_candidate(year: str, chamber: str, state: str, district: str, candidate: str):
-    candidate_info = candidate.split("_")
-    _, _, _, last_name, first_name, _, _ = candidate_info
-    db = load_db(project_dir = project_dir)
-    cleaned_file_path = construct_cleaned_file_path(cleaned_data_dir = cleaned_data_dir, cleaned_file_name = candidate, year = year, chamber = chamber[0], state = state[0], district = district[0])
-    data = covert_data_types(cleaned_file_path = cleaned_file_path)
-    upload_data(db = db, data = data, year = year, chamber = chamber, first_name = first_name, last_name = last_name)
+    split_index = [pos for pos, char in enumerate(candidate) if char == "_"][3]
+    constituency = candidate[:split_index]
+    cleaned_file_name = candidate[split_index + 1:]
+    year, chamber, state, district = constituency.split("_")
+    last_name, first_name, party = cleaned_file_name.split("_")[3:6]
+
+    subject = f"{state}-{district} candidate {first_name} {last_name}"
+
+    db = load_db()
+    cleaned_file_path = construct_cleaned_file_path(cleaned_data_dir, cleaned_file_name, year, chamber, state, district)
+    data = covert_data_types(cleaned_file_path)
+    upload_data(db, data, subject, year, chamber)
