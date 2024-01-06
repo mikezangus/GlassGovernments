4ac463296ebdd47f8ad4d2f0389352e446cd9b72
mikezangus <zangusm@gmail.com>
2024-01-05 19:24:00 -0500

    improved handling for when final house district in state is reached

--- data_collecting/driver.py
+++ data_collecting/driver.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from scraper import scrape_constituency
+from firefox.firefox_driver import firefox_driver
 from modules.sub_modules.log_creator import create_log_file
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,28 +12,35 @@ from user_inputs.driver_user_inputs import get_user_inputs
 
 
 def main():
-    user_inputs_list = get_user_inputs("scrape", "internet")
-    print(f"User inputs list via data collection driver:\n{user_inputs_list}")
-    caffeinate_process = start_caffeinate()
+    caffeinate = start_caffeinate()
     try:
+        driver_loaded, driver = firefox_driver()
+        if not driver_loaded:
+            print("\nQuitting data collection driver\n")
+            return
+        else:
+            driver.quit()
+        constituency_list = get_user_inputs("scrape", "internet")
+        print(f"\nConstituency list via data collection driver:\n{constituency_list}")
         create_log_file()
-        current_state = None
-        for constituency in user_inputs_list:
+        completed_state = None
+        for constituency in constituency_list:
             year, chamber, state = constituency.split("_")[:3]
+            if state == completed_state:
+                continue
             if chamber.lower() == "house":
                 district = constituency.split("_")[3]
             elif chamber.lower() == "senate":
                 district = None
-            if state != current_state:
-                current_state = state
-                constituency_exists = True
-            constituency_exists, driver_not_loaded = scrape_constituency("scrape", year, chamber, state, district)
-            if driver_not_loaded:
-                return
-            elif not constituency_exists:
+            if not scrape_constituency("scrape", year, chamber, state, district):
+                completed_state = state
                 continue
+    except Exception as e:
+        print(f"\nFailed to scrape inputted data. Exception: {e}")
     finally:
-        stop_caffeinate(caffeinate_process)
+        stop_caffeinate(caffeinate)
+        print(f"\nFinished scraping inputted data")
+        print("\nQuitting data collecting driver\n")
 
 
 if __name__ == "__main__":
