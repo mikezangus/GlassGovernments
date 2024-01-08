aeb1786045107bcb9d3daeee8ac82a36d9273847
mikezangus <zangusm@gmail.com>
2024-01-07 23:08:59 -0500

    better handling for problematic rows

--- data_uploading/modules/data_uploader.py
+++ data_uploading/modules/data_uploader.py
@@ -1,22 +1,33 @@
+from datetime import datetime
 from pymongo import GEOSPHERE, UpdateOne
 
 
-def upload_data(db, data, subject: str, year: str, chamber: str):
+def upload_data(db, data, subject, year: str):
     try:
-        collection_name = f"{year}_{chamber.lower()}x"
-        print(f"{subject} | Starting to upload cleaned data to collection {collection_name}")
+        collection_name = f"{year}x"
+        start_time = datetime.now().strftime('%H:%M:%S')
+        print(f"{subject} | Started uploading cleaned data to collection {collection_name} at {start_time}")
         collection = db[collection_name]
         collection.create_index([("contribution_location", GEOSPHERE)])
+        problem_records = []
         operations = []
         for record in data.to_dict(orient = "records"):
+            if not record["transaction_id"]:
+                problem_records.append(record)
+                continue
             operations.append(UpdateOne(
                 {"transaction_id": record["transaction_id"]},
                 {"$setOnInsert": record},
                 upsert = True
             ))
+        if problem_records:
+            print(f"{subject} | Problematic records found in file")
+            for record in problem_records:
+                print(f"{subject} | Missing transaction_id for record: {record}")
         if operations:
             result = collection.bulk_write(operations)
-            print(f"{subject} | Finished uploading {result.upserted_count} new donations to collection {collection_name}")
+            end_time = datetime.now().strftime('%H:%M:%S')
+            print(f"{subject} | Finished uploading {result.upserted_count} new donations to collection {collection_name} at {end_time}")
         else:
             print(f"{subject} | No new donations to upload")
         return
