1cef43c4d6cc6fa73ef6ab8a00b5066457514585
mikezangus <zangusm@gmail.com>
2024-01-10 21:19:19 -0500

    fixed directory

--- data/uploading/modules/mongo_db_connector.py
+++ data/uploading/modules/mongo_db_connector.py
@@ -6,7 +6,8 @@ from pymongo import MongoClient
 def connect_to_mongo_db(subject):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     uploading_dir = os.path.dirname(current_dir)
-    project_dir = os.path.dirname(uploading_dir)
+    data_dir = os.path.dirname(uploading_dir)
+    project_dir = os.path.dirname(data_dir)
     with open(os.path.join(project_dir, "config.json"), "r") as config_file:
         config = json.load(config_file)
     config["uri"] = f"mongodb+srv://{config['mongoUsername']}:{config['mongoPassword']}@{config['mongoCluster']}.px0sapn.mongodb.net/{config['mongoDatabase']}?retryWrites=true&w=majority"
