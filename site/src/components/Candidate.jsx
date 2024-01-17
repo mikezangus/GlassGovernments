b21b69b776312f17711e8561a72f44d85c924ee0
mikezangus <zangusm@gmail.com>
2024-01-16 23:46:24 -0500

    properly deconstructing candidate variables

--- site/src/components/Candidate.jsx
+++ site/src/components/Candidate.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../css/panel.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 
 function getPartyColor (party) {
@@ -16,21 +17,44 @@ function getPartyColor (party) {
 
 function formatCurrency(amount) {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
+};
+
+
+function sortEntities(entities) {
+    return entities.sort((a, b) => b.entityContributionAmount - a.entityContributionAmount);
+};
+
+function calculatePercentage(part, total) {
+    return (part / total * 100).toFixed(2);
+};
+
+
+function createHeatmap ({ data }) {
+
 }
 
 
 export default function DisplayCandidate ({ chamber, state, district, candidate }) {
 
-    const { firstName, lastName, party } = candidate;
+    const { _id: { firstName, lastName, party }, totalContributionAmount } = candidate;
     const [entityContributions, setEntityContributions] = useState([]);
 
     const fetchEntityContributions = async () => {
         try {
-            const response = await fetch(`http://localhost:4000/api/candidate?chamber=${encodeURIComponent(chamber)}&state=${encodeURIComponent(state)}&district=${encodeURIComponent(district)}&firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&party=${encodeURIComponent(party)}`);            console.log("Response: ", response)
+            const params = new URLSearchParams({
+                chamber: chamber,
+                state: state,
+                district: district,
+                firstName: firstName,
+                lastName: lastName,
+                party: party
+            });
+            const url = `http://localhost:4000/api/candidate?${params.toString()}`;
+            const response = await fetch(url);
             if (!response.ok) throw new Error("Network response for display candidate was not ok")
             const data = await response.json();
-            console.log("Data via display candidate component: ", data);
-            setEntityContributions(data);
+            const sortedData = sortEntities(data)
+            setEntityContributions(sortedData);
         } catch (error) {
             console.error("Error fetching candidate data: ", error);
             setEntityContributions([]);
@@ -46,9 +70,9 @@ export default function DisplayCandidate ({ chamber, state, district, candidate 
     const partyColor = candidate ? getPartyColor(party) : "gray";
 
     return (
-        <div className={`panel-candidate ${candidate ? "white-background" : ""}`}>
+        <div className="panel">
             {candidate && (
-                <div className="candidate-info">
+                <div className="info">
                     <h1>
                         {firstName} {lastName}
                     </h1>
@@ -58,13 +82,19 @@ export default function DisplayCandidate ({ chamber, state, district, candidate 
                         </span>
                         , {state}-{district}
                     </h2>
-                    <ul>
+                    <h3>
+                        Total: {formatCurrency(totalContributionAmount)}
                         {entityContributions.map((contribution, index) => (
                             <li key={index}>
-                                {contribution._id}: {contribution.entityContributionAmount}
+                                {contribution._id}: {formatCurrency(contribution.entityContributionAmount)} | {calculatePercentage(contribution.entityContributionAmount, totalContributionAmount)}%
                             </li>
                         ))}
-                    </ul>
+                    </h3>
+                </div>
+            )}
+            {candidate && (
+                <div className="map">
+                    
                 </div>
             )}
         </div>
