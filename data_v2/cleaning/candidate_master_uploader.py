0e7cfa13ad5218ed33e808ed7360936bd56bbbc2
mikezangus <zangusm@gmail.com>
2024-02-03 17:24:01 -0500

    new uploading logic

---
+++ data_v2/cleaning/candidate_master_uploader.py
@@ -0,0 +1,74 @@
+import json
+import os
+import pandas as pd
+import sys
+from pathlib import Path
+from pymongo import MongoClient
+
+cleaning_dir = Path(__file__).resolve().parent
+data_dir = str(cleaning_dir.parent)
+sys.path.append(data_dir)
+from directories import get_config_file_path, get_cleaned_dir
+
+
+def decide_year() -> str:
+    year_dirs = os.listdir(get_cleaned_dir())
+    year_options = [y for y in year_dirs if y.isdigit()]
+    sorted_year_options = sorted(
+        year_options,
+        key = lambda x: int(x)
+    )
+    formatted_year_options = ', '.join(sorted_year_options)
+    while True:
+        year = input(f"\nFor which year do you want to clean raw data?\nAvailable years: {formatted_year_options}\n> ")
+        if year in year_options:
+            return year
+        else:
+            print(f"\n{year} isn't an available year, try again")
+
+
+def get_input_file_path(year: str) -> str:
+    path = os.path.join(get_cleaned_dir(), year, "candidate_master.csv")
+    return path
+
+
+def connect_to_mongo():
+    path = get_config_file_path()
+    with open(path, "r") as config_file:
+        config = json.load(config_file)
+    uri = f"mongodb+srv://{config['mongoUsername']}:{config['mongoPassword']}@{config['mongoCluster']}.px0sapn.mongodb.net/{config['mongoDatabase']}?retryWrites=true&w=majority"
+    config["uri"] = uri
+    client = MongoClient(config["uri"])
+    db = client[config["mongoDatabase"]]
+    print("Successfully connected to MongoDB")
+    return db
+
+
+def load_df(path: str) -> pd.DataFrame:
+    df = pd.read_csv(
+        filepath_or_buffer = path,
+        sep = ",",
+        dtype = str
+    )
+    return df
+
+
+def upload_df(year: str, db, df: pd.DataFrame) -> None:
+    collection_name = f"{year}_candidate_master"
+    collection = db[collection_name]
+    records = df.to_dict("records")
+    collection.insert_many(records)
+    print(f"Finished uploading {len(records)} records to collection {collection_name}")
+
+
+def main():
+    year = decide_year()
+    input_file_path = get_input_file_path(year)
+    db = connect_to_mongo()
+    df = load_df(input_file_path)
+    upload_df(year, db, df)
+
+
+
+if __name__ == "__main__":
+    main()
--- data_v2/directories.py
+++ data_v2/directories.py
@@ -1,9 +1,16 @@
 import os
+from pathlib import Path
 
 
 def verify_dir_exists(dir: str):
     if not os.path.exists(dir):
         os.makedirs(dir, exist_ok = True)
+
+
+def get_project_dir() -> str:
+    directories_dir = Path(__file__).resolve().parent
+    project_dir = directories_dir.parent
+    return project_dir
 
 
 def get_data_files_dir():
@@ -35,3 +42,9 @@ def get_src_file_dir(year: str, file_type: str):
     dir = os.path.join(get_raw_dir(), year, file_type)
     verify_dir_exists(dir)
     return dir
+
+
+def get_config_file_path() -> str:
+    dir = get_project_dir()
+    path = os.path.join(dir, "config.json")
+    return path
