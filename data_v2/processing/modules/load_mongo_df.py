b314fbaff9ba4a33620f2af9d38a0a8c335d3463
mikezangus <zangusm@gmail.com>
2024-02-06 23:01:35 -0500

    rename and multiple field handling

---
+++ data_v2/processing/modules/load_mongo_df.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession, DataFrame
+
+
+def load_mongo_df(year: str, collection_name: str, spark: SparkSession, uri: str, subject: str, field_1: str, field_2: str = None) -> DataFrame:
+    collection = f"{year}_{collection_name}"
+    print(f"\Started loading {subject} DataFrame from collection {collection}")
+    df = spark.read \
+        .format("mongo") \
+        .option("uri", uri) \
+        .option("collection", collection) \
+        .load()
+    if df.limit(1).count() > 0:
+        if field_2:
+            df = df.select(field_1, field_2)
+        else:
+            df = df.select(field_1)
+        print(f"Finished loading {subject} DataFrame\nItem count: {df.count():,}")
+        return df
+    print(f"{collection} is empty or doesn't exist")
+    return None
