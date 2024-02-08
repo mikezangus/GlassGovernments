7c981efa221da8d06f21eb225dd41fd8c64eced6
mikezangus <zangusm@gmail.com>
2024-02-07 20:11:17 -0500

    indexing for efficient queries

---
+++ site/server/createIndexes.js
@@ -0,0 +1,36 @@
+const { getDB } = require("./mongoClient");
+
+
+async function createIndexes(years) {
+    console.log("entered create indexes")
+    const db = getDB();
+    for (const year of years) {
+        try {
+            await db
+                .collection(`${year}_candidates`)
+                .createIndex({
+                    OFFICE: 1,
+                    STATE: 1,
+                    DISTRICT: 1
+                });
+            console.log("Successfully created index 1");
+            await db
+                .collection(`${year}_candidates`)
+                .createIndex({
+                    CAND_ID: 1
+                });
+            console.log("Successfully created index 2");
+            await db
+                .collection(`${year}_contributions`)
+                .createIndex({
+                    CAND_ID: 1
+                });
+            console.log("Successfully created index 3");
+        } catch (err) {
+            console.error("Error creating indexes:", err);
+        };
+    };
+};
+
+
+module.exports = createIndexes;
