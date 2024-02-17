66bb2372949405a06971578344bbac7b507dc467
mikezangus <zangusm@gmail.com>
2024-02-17 13:21:31 -0500

    put options into its own function for cleanliness

--- data/collecting/firefox/firefox_driver.py
+++ data/collecting/firefox/firefox_driver.py
@@ -6,28 +6,12 @@ from .firefox_app_downloader import download_firefox_app
 from .geckodriver_downloader import download_geckodriver
 
 
+def set_options(binary_path: str, downloads_dir: str) -> Options:
 
-def firefox_driver(download_dir: str):
-
-    firefox_dir = os.path.dirname(os.path.abspath(__file__))
-    firefox_binary_path = os.path.join(firefox_dir, "Firefox Developer Edition.app", "Contents", "MacOS", "firefox")
-    geckodriver_path = os.path.join(firefox_dir, "geckodriver")
-    
-    if not os.path.isfile(firefox_binary_path):
-        if not download_firefox_app():
-            return False, None
-    for file_name in os.listdir(firefox_dir):
-        if file_name.lower().startswith("firefox_latest"):
-            os.remove(os.path.join(firefox_dir, file_name))
-    if not os.path.isfile(geckodriver_path):
-        if not download_geckodriver():
-            return False, None
-    geckodriver_log_path = os.path.join(firefox_dir, "geckodriver.log")
-    
     options = Options()
-    options.binary_location = firefox_binary_path
+    options.binary_location = binary_path
 
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
    
     options.set_preference("browser.cache.disk.enable", False)
     options.set_preference("browser.cache.memory.enable", False)
@@ -36,7 +20,7 @@ def firefox_driver(download_dir: str):
     options.set_preference("network.http.use-cache", False)
 
     options.set_preference("browser.download.alwaysOpenPanel", False)
-    options.set_preference("browser.download.dir", download_dir)
+    options.set_preference("browser.download.dir", downloads_dir)
     options.set_preference("browser.download.folderList", 2)
     options.set_preference("browser.download.manager.showAlertOnComplete", False)
     options.set_preference("browser.download.manager.showWhenStarting", False)
@@ -46,7 +30,31 @@ def firefox_driver(download_dir: str):
 
     options.set_preference("layout.css.devPixelsPerPx", "1")
 
-    driver = webdriver.Firefox(executable_path = geckodriver_path, firefox_options = options, log_path = geckodriver_log_path)
+    return options
+
+
+def firefox_driver(downloads_dir: str) -> tuple[bool, webdriver.Firefox]:
+
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    binary_path = os.path.join(app_dir, "Firefox Developer Edition.app", "Contents", "MacOS", "firefox")
+    geckodriver_path = os.path.join(app_dir, "geckodriver")
+    
+    if not os.path.isfile(binary_path):
+        if not download_firefox_app():
+            return False, None
+    for file_name in os.listdir(app_dir):
+        if file_name.lower().startswith("firefox_latest"):
+            os.remove(os.path.join(app_dir, file_name))
+    if not os.path.isfile(geckodriver_path):
+        if not download_geckodriver():
+            return False, None
+    geckodriver_log_path = os.path.join(app_dir, "geckodriver.log")
+    
+    options = set_options(binary_path, downloads_dir)
+
+    driver = webdriver.Firefox(
+        executable_path = geckodriver_path, options = options, log_path = geckodriver_log_path
+    )
     
     driver.maximize_window()
     return True, driver
