6043dbfbc814ece49e647805aabf5eb736dc71e6
mikezangus <zangusm@gmail.com>
2024-02-24 23:51:22 -0500

    shared logic for processing different types of contributions

---
+++ data/processing/contribution_types/committees.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+file_types_dir = os.path.dirname(os.path.abspath(__file__))
+processing_dir = os.path.dirname(file_types_dir)
+sys.path.append(processing_dir)
+from process_contributions import process_contributions
+
+
+def process_committee_conts(year: str = None) -> None:
+    process_contributions("Committee", "pas2", "itpas2.txt", year)
+    return
+
+
+if __name__ == "__main__":
+    process_committee_conts()
---
+++ data/processing/process_contributions.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+file_types_dir = os.path.dirname(os.path.abspath(__file__))
+processing_dir = os.path.dirname(file_types_dir)
+sys.path.append(processing_dir)
+from modules.decide_year import decide_year
+from modules.get_mongo_uri import get_mongo_uri
+from modules.load_df_from_file import load_df_from_file
+from modules.load_df_from_mongo import load_df_from_mongo
+from modules.load_headers import load_headers
+from modules.load_spark import load_spark
+from modules.rename_cols import rename_cont_cols
+from modules.sanitize_df import sanitize_df
+from modules.set_cols import set_cont_cols
+from modules.upload_df import upload_df
+from modules.contributions.convert_to_coords import main as convert_to_coords
+from modules.contributions.format_df import format_df
+from modules.contributions.join_dfs import join_dfs
+
+
+def process_contributions(subject: str, file_type: str, file_name: str, year: str = None) -> None:
+
+    print(f"\n{'-' * 100}\n{'-' * 100}\nStarted processing {subject} Contributions")
+
+    if not year:
+        year = decide_year()
+
+    uri = get_mongo_uri()
+    spark = load_spark(uri)
+    headers = load_headers(file_type)
+    cols = set_cont_cols("input", headers)
+
+    main_df = load_df_from_file(year, file_type, file_name, spark, headers, cols)
+    existing_items_df = load_df_from_mongo(spark, uri, f"{year}_conts", "Existing Items", "TRAN_ID")
+    if existing_items_df is not None:
+        main_df = join_dfs(main_df, existing_items_df, "TRAN_ID", "left_anti", "Filtering out existing items")
+    cands_df = load_df_from_mongo(spark, uri, f"{year}_cands", "Candidates", "CAND_ID", "CMTE_ID")
+    main_df = join_dfs(main_df, cands_df, "CMTE_ID", "inner", "Filtering out ineligible candidates")
+    main_df = rename_cont_cols(main_df)
+    main_df = format_df(main_df)
+    main_df = convert_to_coords(spark, main_df)
+    main_df = sanitize_df(main_df, "cont")
+    upload_df(f"{year}_conts", uri, main_df, "overwrite")
+
+    spark.stop()
+    
+    print(f"\nFinished processing {subject} Contributions\n{'-' * 100}\n{'-' * 100}\n")
+
+    return
+
+
+def main(year: str = None):
+    if not year:
+        year = decide_year()
+    process_contributions("Committee", "pas2", "itpas2.txt", year)
+    process_contributions("Other", "oth", "itoth.txt", year)
+    process_contributions("Individual", "indiv", "itcont.txt", year)
+
+
+if __name__ == "__main__":
+    main()
