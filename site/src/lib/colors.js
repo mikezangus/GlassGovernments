50011e4701e63df3c4ba34116bfc43a241427673
mikezangus <zangusm@gmail.com>
2024-03-06 01:18:19 -0500

    trying lighter brown

--- site/src/lib/colors.js
+++ site/src/lib/colors.js
@@ -5,4 +5,4 @@ export const greenOpaque = `rgb(${green})`;
 export const greenTransparent = `rgba(${green}, 0.5)`;
 
 export const brownOpaque = `rgb(${brown})`;
-export const brownTransparent = `rgba(${brown}, 0.5)`;
+export const brownTransparent = `rgba(${brown}, 0.4)`;
