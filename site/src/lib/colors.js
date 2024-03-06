aaa5a7b4f767cd1fe3f653f24250d1731ede25ba
mikezangus <zangusm@gmail.com>
2024-03-05 22:34:35 -0500

    colors set in own file so legend and graph are synced

---
+++ site/src/lib/colors.js
@@ -0,0 +1,8 @@
+export const green = "47, 255, 141";
+export const brown = "60, 27, 5";
+
+export const greenOpaque = `rgb(${green})`;
+export const greenTransparent = `rgba(${green}, 0.5)`;
+
+export const brownOpaque = `rgb(${brown})`;
+export const brownTransparent = `rgba(${brown}, 0.5)`;
