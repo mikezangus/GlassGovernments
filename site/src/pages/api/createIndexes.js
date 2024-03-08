62fd4819f367fef2d0973f98377f12f878f8903f
mikezangus <zangusm@gmail.com>
2024-03-07 22:53:26 -0500

    updated indexing

--- site/src/pages/api/createIndexes.js
+++ site/src/pages/api/createIndexes.js
@@ -2,29 +2,51 @@ import getDB from "../../lib/mongoClient";
 import fetchYears from "../../lib/fetchYears";
 
 
+async function createCandsIndex(db, year) {
+    try {
+        await db
+            .collection(`${year}_cands`)
+            .createIndex({
+                OFFICE: 1,
+                STATE: 1,
+                DISTRICT: 1,
+                CAND_ID: 1
+            });
+    } catch (err) {
+        console.error(`Create Candidate Index ${year} | Error: ${err}`);
+    }
+};
+
+
+async function createContsIndex(db, year) {
+    try {
+        await db
+            .collection(`${year}_conts`)
+            .createIndex({
+                CAND_ID: 1,
+                STATE: 1,
+                CITY: 1
+            })
+        await db
+            .collection(`${year}_conts`)
+            .createIndex({
+                CAND_ID: 1,
+                "LOCATION.coordinates": "2dsphere"
+            })
+    } catch (err) {
+        console.error(`Create Contributions Index ${year} | Error: ${err}`);
+    }
+};
+
+
 async function createIndexes(db) {
     const years = await fetchYears(db);
     for (const year of years) {
         try {
-            await Promise.all([
-                db
-                    .collection(`${year}_cands`)
-                    .createIndex(
-                        {
-                            OFFICE: 1,
-                            STATE: 1,
-                            DISTRICT: 1
-                        }
-                    ),
-                db
-                    .collection(`${year}_cands`)
-                    .createIndex({ CAND_ID: 1 }),
-                db
-                    .collection(`${year}_conts`)
-                    .createIndex({ CAND_ID: 1 })
-            ]);
+            await createCandsIndex(db, year);
+            await createContsIndex(db, year);
         } catch (err) {
-            console.error(`Create Indexes (${year}) | Error: `, err);
+            console.error(`Create Indexes ${year} | Error: ${err}`);
         }
     }
 };
