bbc560dd4c446b0bddcd11c94a58203d4d79bbbc
mikezangus <zangusm@gmail.com>
2024-03-17 00:53:27 -0400

    don't see a need to define mongo indexes in the react app

--- site/src/pages/_app.jsx
+++ site/src/pages/_app.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "leaflet/dist/leaflet.css";
 import Header from "../components/Header";
 import "../styles/index.css";
 
 
 export default function App({ Component, pageProps }) {
-
-    const createIndexes = async() => {
-        const response = await fetch(
-            "/api/createIndexes",
-            { method: "POST" }
-        );
-    };
-    useEffect(() => createIndexes(), []);
-
     return (
         <>
         <Header />
