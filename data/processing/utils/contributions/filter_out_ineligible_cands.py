f0953deea4413de01aeda91f034b906c7dc5bf3d
mikezangus <zangusm@gmail.com>
2024-03-16 23:00:26 -0400

    moved to own file

---
+++ data/processing/utils/contributions/filter_out_ineligible_cands.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from pyspark.sql import DataFrame, SparkSession
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+UTILS_DIR = os.path.dirname(CURRENT_DIR)
+sys.path.append(UTILS_DIR)
+from join_dfs import join_dfs
+from load_df_from_mongo import load_df_from_mongo
+
+
+def filter_out_ineligible_cands(
+    file_type: str,
+    uri: str,
+    year: str,
+    spark: SparkSession,
+    conts_df: DataFrame
+) -> DataFrame | None:
+    if file_type == "pas2":
+        cands_df = load_df_from_mongo(
+            "spark",
+            uri,
+            f"{year}_cands_raw",
+            spark = spark,
+            fields = ["CAND_ID"],
+            subject = "Candidates"
+        )
+        df = join_dfs(
+            conts_df,
+            cands_df,
+            "CAND_ID",
+            "inner",
+            "filtering out ineligible candidates"
+        )
+    else:
+        cmtes_df = load_df_from_mongo(
+            "spark",
+            uri,
+            f"{year}_cmtes",
+            spark = spark,
+            fields = ["CAND_ID", "CMTE_ID"],
+            subject = "Candidates"
+        )
+        df = join_dfs(
+            conts_df,
+            cmtes_df,
+            "CMTE_ID",
+            "inner",
+            "filtering out ineligible candidates"
+        )
+
+    if df.limit(1).count() == 0:
+        print("No items to upload, exiting")
+        return
+    return df
