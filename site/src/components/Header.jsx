f1f90c7fcdc1533f4c231f0a505b6eda935f622c
mikezangus <zangusm@gmail.com>
2024-03-17 22:29:23 -0400

    site formatting

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -13,14 +13,14 @@ export default function Header() {
                 </div>
                 <div className={styles.pagesContainer}>
                     <a
-                        href="https://glassgovernments.com/about"
+                        href="https://github.com/mikezangus/glassgovernments"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         About
                     </a>
                     <a
-                        href="https://google.com"
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSf3hDdCeb_FaMb8p1qCjSwm8nGu-o_vDhWH5sKNbg6LEEC8oQ/viewform?usp=sf_link"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
