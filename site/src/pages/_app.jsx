f3fde634380b7a683a01593658231d4a2dc22579
mikezangus <zangusm@gmail.com>
2024-03-19 14:46:43 -0400

    added vercel speed insights

--- site/src/pages/_app.jsx
+++ site/src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"; 
 import Header from "../components/Header";
 import "leaflet/dist/leaflet.css";
 import "../styles/index.css";
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <Analytics />
+            <SpeedInsights />
             <Header />
             <main>
                 <Component {...pageProps} />
