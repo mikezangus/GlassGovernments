ba38d7359baf16fb3a005ea872f9fbebc0b829a5
mikezangus <zangusm@gmail.com>
2025-04-10 01:04:46 -0400

    deletions

--- site/src/lib/colors.js
+++
@@ -1,8 +0,0 @@
-export const green = "45, 255, 140";
-export const brown = "100, 30, 5";
-
-export const greenOpaque = `rgb(${green})`;
-export const greenTransparent = `rgba(${green}, 0.35)`;
-
-export const brownOpaque = `rgb(${brown})`;
-export const brownTransparent = `rgba(${brown}, 0.35)`;
--- site/src/lib/showStateName.js
+++
@@ -1,7 +0,0 @@
-import usa_states_all from "./usa_states_all.json";
-
-
-export default function showStateName(stateCode) {
-    const stateName = usa_states_all[stateCode] || stateCode;
-    return stateName
-};
