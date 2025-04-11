1af2d2d03b407c9b67e67ba432b3bfa461cca690
mikezangus <zangusm@gmail.com>
2025-04-11 01:22:38 -0400

    filter out null values

--- pipelines/bill-metadata/bases/createRow.ts
+++ pipelines/bill-metadata/bases/createRow.ts
@@ -4,20 +4,21 @@ import { RawBill, Bill } from "../types";
 export default function createRow(data: RawBill[]): Bill[]
 {
     return data
-        .filter((item) => {item.congress && item.type && item.number})
+        .filter(item => item.congress && item.type && item.number)
         .map((item) => {
-        const id = `${item.congress}_${item.type}_${item.number}`;
-        const congress = Number(item.congress);
-        return {
-            id,
-            congress: congress,
-            type: item.type,
-            num: item.number,
-            title: item.title,
-            h_vote: 0,
-            h_year: 0,
-            s_vote: 0,
-            s_session: 0
+            const id = `${item.congress}_${item.type}_${item.number}`;
+            const congress = Number(item.congress);
+            return {
+                id,
+                congress: congress,
+                type: item.type,
+                num: item.number,
+                title: item.title,
+                h_vote: 0,
+                h_year: 0,
+                s_vote: 0,
+                s_session: 0
+            }
         }
-    });
+    );
 }
