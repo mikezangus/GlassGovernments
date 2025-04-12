8ffaeb206f680c96fb2ef5cb95237c8d2bf9173f
mikezangus <zangusm@gmail.com>
2025-04-11 23:33:34 -0400

    naming

--- pipelines/bill-texts/loadFromFiles.ts
+++ pipelines/bill-texts/loadFromFiles.ts
@@ -3,15 +3,16 @@ import path from "path";
 import { BillText } from "./types";
 
 
-const DIR = path.resolve(__dirname, "../data/bill-texts");
+const DIR = path.resolve(__dirname, "../../data/bill-texts");
 
 
-export default async function fetchFromFiles(): Promise<BillText[]>
+export default async function loadFromFiles(): Promise<BillText[]>
 {
     const data: BillText[] = [];
     const fileNames = await fs.readdir(DIR);
     for (const fileName of fileNames) {
         try {
+            console.log(fileName);
             const filePath = path.join(DIR, fileName);
             const stat = await fs.stat(filePath);
             if (!stat.isFile()
