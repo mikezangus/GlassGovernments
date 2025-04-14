d2ccca7af82a6b4c5dee020e7fcc69c763ec56c5
mikezangus <zangusm@gmail.com>
2025-04-13 23:58:20 -0400

    created

---
+++ pipelines/utils/insertToDB.ts
@@ -0,0 +1,22 @@
+import pool from "../../db";
+
+
+export default async function insertToDB(
+    data: any[],
+    tableName: string,
+    query: string,
+    paramMapper: (row: any) => []
+): Promise<void>
+{
+    console.log(`Inserting to ${tableName}`);
+    let affected = 0;
+    for (const row of data) {
+        try {
+            const result = await pool.query(query, paramMapper(row));
+            affected += result.rowCount ?? 0;
+        } catch (err) {
+            console.error(`⚠️ Error: ${err}\nRow: ${row}\n`);
+        }
+    }
+    console.log(`Inserted ${affected} rows to ${tableName}`);
+}
