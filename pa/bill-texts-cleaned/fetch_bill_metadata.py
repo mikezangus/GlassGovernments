626b3589f238faee994c237f605fbd2cd61d4fe1
mikezangus <zangusm@gmail.com>
2025-04-27 14:49:27 -0400

    fetching and creating dicts in main

--- pa/bill-texts-cleaned/fetch_bill_metadata.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(CURRENT_DIR))
-from fetch_from_db import fetch_from_db
-
-
-def fetch_bill_metadata() -> list:
-    return fetch_from_db("pa_bill_metadata", { "select": '*' })
--- pa/bill-texts-cleaned/main.py
+++ pa/bill-texts-cleaned/main.py
@@ -1,29 +1,37 @@
 import os
 import sys
-from tokenize_text import tokenize_text
-from find_common_tokens import find_common_tokens
+from clean_tokens import clean_tokens
+from find_universal_tokens import find_universal_tokens
 from group_by_type import group_by_type
-from fetch_bill_metadata import fetch_bill_metadata
+from tokenize_text import tokenize_text
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
+from fetch_from_db import fetch_from_db
 from insert_to_db import insert_to_db
 
 
 def main():
-    bill_metadata = fetch_bill_metadata()
-    tokenized = tokenize_text(bill_metadata)
-    grouped_by_type = group_by_type(tokenized)
+    metadata = fetch_from_db("pa_bill_metadata", { "select": '*' })
+    texts = fetch_from_db("pa_bill_texts_flattened", { "select": '*' })
+    id_to_text = {}
+    for row in texts:
+        id_to_text[row["id"]] = row["text"]
+    metadata_and_texts = []
+    for row in metadata:
+        enriched = row.copy()
+        enriched["text"] = id_to_text.get(row["id"], "")
+        metadata_and_texts.append(enriched)
+    metadata_and_tokens = tokenize_text(metadata_and_texts)
+    type_to_metadata_and_tokens = group_by_type(metadata_and_tokens)
     cleaned = []
-    for _, bills in grouped_by_type.items():
-        common_words = find_common_tokens(bills)
+    for type, bills in type_to_metadata_and_tokens.items():
+        print(f"\n{type}")
+        universal_tokens = find_universal_tokens(bills)
         for bill in bills:
-            uncommon_words = bill["tokens"] - set(common_words)
-            uncommon_words.discard(bill["id"].lower())
-            uncommon_words.discard(str(bill["bill_num"]))
-            uncommon_words.discard(str(bill["print_num"]))
+            tokens = clean_tokens(bill, universal_tokens)
             cleaned.append({
                 "id": bill["id"],
-                "tokens": list(uncommon_words)
+                "tokens": tokens
             })
     insert_to_db("pa_bill_texts_cleaned", cleaned)
     
