ed71ca06704b78a32cb342db3570eb3b0628a10a
mikezangus <zangusm@gmail.com>
2025-04-27 14:50:14 -0400

    formatting

--- pa/bill-texts-cleaned/group_by_type.py
+++ pa/bill-texts-cleaned/group_by_type.py
@@ -1,8 +1,10 @@
-def group_by_type(bills: list[dict]) -> dict[str, list[dict]]:
-    split = {}
+def group_by_type(
+    bills: list[dict[str, object]]
+) -> dict[str, list[dict[str, object]]]:
+    grouped = {}
     for bill in bills:
         bill_type = bill["bill_type"]
-        if bill_type not in split:
-            split[bill_type] = []
-        split[bill_type].append(bill)
-    return split
+        if bill_type not in grouped:
+            grouped[bill_type] = []
+        grouped[bill_type].append(bill)
+    return grouped
