9818e5c7d73cf208849478ae608ea1fcad155a0e
mikezangus <zangusm@gmail.com>
2025-05-11 23:47:28 -0400

    get url from column instead of constructing it

--- pipelines/pa/bill-texts-source/main.py
+++ pipelines/pa/bill-texts-source/main.py
@@ -4,38 +4,35 @@ import os
 import sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATE_DIR = os.path.dirname(CURRENT_DIR)
-sys.path.append(os.path.dirname(STATE_DIR))
-from urls import bill_text_url
 PIPELINES_DIR = os.path.dirname(STATE_DIR)
-sys.path.append(os.path.dirname(PIPELINES_DIR))
+sys.path.append(PIPELINES_DIR)
 from fetch_from_db import fetch_from_db
 from insert_to_db import insert_to_db
 
 
 def main():
-    rows = fetch_from_db("pa_bill_metadata", query_params={ "select": '*' })
-    texts = []
-    for (i, row) in enumerate(rows):
-        print(f"[{i + 1}/{len(rows)}]", row["id"])
+    input_rows = fetch_from_db(
+        "bill_metadata",
+        query_params={
+            "select": "id,text_url",
+            "state": "eq.PA"
+        }
+    )
+    if not input_rows:
+        print("❌ Failed to fetch from DB")
+    output_rows = []
+    for (i, input_row) in enumerate(input_rows):
+        print(f"[{i + 1}/{len(input_rows)}]", input_row["id"])
         try:
-            html = fetch_html_from_web(
-                bill_text_url(
-                    row["year"],
-                    row["session"],
-                    row["bill_type"],
-                    row["bill_num"],
-                    row["print_num"]
-                )
-            )
+            html = fetch_html_from_web(input_row["text_url"])
         except:
-            print('❌ Failed to fetch HTML')
+            print("❌ Failed to fetch HTML")
             continue
-        text = extract_text_from_html(html)
-        texts.append({
-            "id": row["id"],
-            "text": text
+        output_rows.append({
+            "id": input_row["id"],
+            "text": extract_text_from_html(html)
         })
-    insert_to_db("pa_bill_texts_source", texts)
+    insert_to_db("bill_texts_source", output_rows)
 
 
 if __name__ == "__main__":
