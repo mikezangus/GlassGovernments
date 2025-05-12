3b9e7fe8c9caeadf15052b1341411606cfb6fde3
mikezangus <zangusm@gmail.com>
2025-05-11 23:47:59 -0400

    updated table

--- pipelines/pa/bill-texts-flattened/main.py
+++ pipelines/pa/bill-texts-flattened/main.py
@@ -3,21 +3,23 @@ import os
 import sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PIPELINES_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
-sys.path.append(os.path.dirname(PIPELINES_DIR))
+sys.path.append(PIPELINES_DIR)
 from fetch_from_db import fetch_from_db
 from insert_to_db import insert_to_db
 
 
 def main():
     rows = fetch_from_db(
-        "pa_bill_texts_source",
+        "bill_texts_source",
         query_params={"select": '*'}
     )
     flattened_rows = []
     for row in rows:
-        flattened_text = flatten_text(row["text"])
-        flattened_rows.append({ "id": row["id"], "text": flattened_text })
-    insert_to_db("pa_bill_texts_flattened", flattened_rows)
+        flattened_rows.append({
+            "id": row["id"],
+            "text": flatten_text(row["text"])
+        })
+    insert_to_db("bill_texts_flat", flattened_rows)
 
 
 if __name__ == "__main__":
