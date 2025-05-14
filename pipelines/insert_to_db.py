8d34a05defb9fedb5385bd562ac591f379a2ddee
mikezangus <zangusm@gmail.com>
2025-05-13 20:31:37 -0400

    duplicate insert handling

--- pipelines/insert_to_db.py
+++ pipelines/insert_to_db.py
@@ -1,13 +1,28 @@
 import requests
 from db import supabase_headers, supabase_url
+from enum import Enum
 
 
-def insert_to_db(table_name: str, data: list[dict]) -> None:
+class OnDuplicate(Enum):
+    IGNORE = "ignore"
+    MERGE = "merge"
+
+
+def insert_to_db(
+    table_name: str,
+    data: list[dict],
+    on_duplicate: OnDuplicate,
+    conflict_key: str | None = None
+) -> None:
     print(f"\nInserting to {table_name}")
+    if on_duplicate == OnDuplicate.MERGE and not conflict_key:
+        raise ValueError("conflict_key must be provided when using OnDuplicate.MERGE")
     url = f"{supabase_url}/rest/v1/{table_name}"
+    if conflict_key:
+        url += f"?on_conflict={conflict_key}"
     headers = {
         **supabase_headers,
-        "Prefer": "resolution=ignore-duplicates,return=representation"
+        "Prefer": f"resolution={on_duplicate.value}-duplicates,return=representation"
     }
     response = requests.post(url, headers=headers, json=data)
     if response.ok:
