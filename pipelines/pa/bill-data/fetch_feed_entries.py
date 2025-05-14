a7586f0e8fcc27921221fa11f3b8e33f3eb890e9
mikezangus <zangusm@gmail.com>
2025-05-13 23:43:27 -0400

    formatting

--- pipelines/pa/bill-data/fetch_feed_entries.py
+++ pipelines/pa/bill-data/fetch_feed_entries.py
@@ -2,6 +2,6 @@ import feedparser
 from feedparser import FeedParserDict
 
 
-def fetch_rss_entries(url: str) -> list[FeedParserDict]:
+def fetch_feed_entries(url: str) -> list[FeedParserDict]:
     feed = feedparser.parse(url)
     return list(feed.entries)
