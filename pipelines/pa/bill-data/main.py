eeb5936d5d80cea1a6dbeb280718d95f16e1422f
mikezangus <zangusm@gmail.com>
2025-05-13 23:43:22 -0400

    standalone urls file

--- pipelines/pa/bill-data/main.py
+++ pipelines/pa/bill-data/main.py
@@ -1,4 +1,4 @@
-from fetch_rss_entries import fetch_rss_entries
+from fetch_feed_entries import fetch_feed_entries
 from parse_actions import parse_actions
 from parse_metadata import parse_metadata
 import os
@@ -8,20 +8,16 @@ STATE_DIR = os.path.dirname(CURRENT_DIR)
 PIPELINES_DIR = os.path.dirname(STATE_DIR)
 sys.path.append(PIPELINES_DIR)
 from insert_to_db import insert_to_db, OnDuplicate
-
-
-rss_base_url = "https://legis.state.pa.us/WU01/LI/RSS/"
-house_rss_url = rss_base_url + "HouseBills.xml"
-senate_rss_url = rss_base_url + "SenateBills.xml"
+from urls import house_rss_url, senate_rss_url
 
 
 def main():
-    rss_entries = []
+    feed_entries = []
     for url in [house_rss_url, senate_rss_url]:
-        rss_entries.extend(fetch_rss_entries(url))
+        feed_entries.extend(fetch_feed_entries(url))
     metadata_rows = []
     actions_rows = []
-    for entry in rss_entries:
+    for entry in feed_entries:
         metadata_rows.append(parse_metadata(entry))
         actions_rows.append(parse_actions(entry))
     insert_to_db("bill_metadata", metadata_rows, OnDuplicate.IGNORE, "id")
---
+++ pipelines/pa/urls.py
@@ -0,0 +1,3 @@
+rss_base_url = "https://legis.state.pa.us/WU01/LI/RSS/"
+house_rss_url = rss_base_url + "HouseBills.xml"
+senate_rss_url = rss_base_url + "SenateBills.xml"
