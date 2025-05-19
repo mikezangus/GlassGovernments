9586c33f8e3a1a9121e2db53963be01d949e257b
mikezangus <zangusm@gmail.com>
2025-05-18 22:52:49 -0400

    multiple conflict cols

--- site/src/lib/insertToDB.ts
+++ site/src/lib/insertToDB.ts
@@ -2,14 +2,22 @@ import { db } from "./db";
 
 
 export default async function insertToDB<T extends Record<string, unknown>>(
-    table: string, rows: T[], conflictColumn?: string
+    tableName: string,
+    rows: T[],
+    conflictColumns?: string | string[]
 ): Promise<void>
 {
-    const query = conflictColumn
-            ? db.from(table).upsert(rows, { onConflict: conflictColumn })
-            : db.from(table).insert(rows);
+    if (rows.length === 0) {
+        return;
+    }
+    if (conflictColumns && Array.isArray(conflictColumns)) {
+        conflictColumns = conflictColumns.join(",");
+    }
+    const query = conflictColumns
+            ? db.from(tableName).upsert(rows, { onConflict: conflictColumns })
+            : db.from(tableName).insert(rows);
     const { error } = await query;
     if (error) {
-        throw error
+        throw new Error(`Error inserting to ${tableName}: ${error.message}`);
     }
 }
