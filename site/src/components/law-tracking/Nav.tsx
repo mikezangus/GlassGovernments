c9bc44833b2758b51ad379a800f5f257fae25d74
mikezangus <zangusm@gmail.com>
2025-05-31 19:53:19 -0400

    created

---
+++ site/src/components/law-tracking/Nav.tsx
@@ -0,0 +1,73 @@
+import { Step, TokenItem } from "./types";
+import styles from "@/styles/LawTracking.module.css";
+
+
+function handleForward(
+    currentStep: Step,
+    setCurrentStep: (step: Step) => void,
+    tokenItems: TokenItem[],
+    setTokenItems: (tokenItems: TokenItem[]) => void,
+): void
+{
+    if (currentStep === Step.Tokens) {
+        const filteredTokens = tokenItems.filter(t => t.token.trim() !== "");
+        setTokenItems(filteredTokens);
+    }
+    if (currentStep < Step.Contact) {
+        setCurrentStep(currentStep + 1);
+    }
+}
+
+
+function handleBackward(
+    currentStep: Step,
+    setCurrentStep: (step: Step) => void
+): void
+{
+    if (currentStep > Step.Tokens) {
+        setCurrentStep(currentStep - 1);
+    } 
+}
+
+export default function NavComponent(
+    {
+        currentStep,
+        setCurrentStep,
+        tokenItems,
+        setTokenItems
+    }:
+    {
+        currentStep: Step;
+        setCurrentStep: (step: Step) => void;
+        tokenItems: TokenItem[];
+        setTokenItems: (tokenItems: TokenItem[]) => void;
+    }
+)
+{
+    return (
+        <div className={styles.navigationButtons}>
+            <button
+                onClick={() => handleBackward(
+                    currentStep,
+                    setCurrentStep
+                )}
+                disabled={currentStep === Step.Tokens}
+                className={styles.navButton}
+            >
+                {"<-"}
+            </button>
+            <button
+                onClick={() => handleForward(
+                    currentStep,
+                    setCurrentStep,
+                    tokenItems,
+                    setTokenItems
+                )}
+                disabled={currentStep === Step.Contact}
+                className={styles.navButton}
+            >
+                {"->"}
+            </button>
+        </div>
+    );
+}
