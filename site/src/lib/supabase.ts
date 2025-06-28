3e2eeb3d033dc90ecd981e7e645b9ac03ce0c96b
mikezangus <zangusm@gmail.com>
2025-06-28 19:47:43 -0400

    changed name

--- site/src/lib/supabase.ts
+++ site/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-export const db = createClient(
+export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_KEY!
 );
