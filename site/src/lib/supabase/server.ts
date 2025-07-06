b06b61e51226a5ce4f31e4e528b6d4e1c3edc6a1
mikezangus <zangusm@gmail.com>
2025-07-06 14:54:30 -0400

    typo

--- site/src/lib/supabase/server.ts
+++ site/src/lib/supabase/server.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_API_URL!,
+    process.env.SUPABASE_API_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
