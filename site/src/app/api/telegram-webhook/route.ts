06c866987ab5f23666d90f1bdd57b73a519d6b91
mikezangus <zangusm@gmail.com>
2025-07-06 15:12:23 -0400

    console logs

--- site/src/app/api/telegram-webhook/route.ts
+++ site/src/app/api/telegram-webhook/route.ts
@@ -4,8 +4,11 @@ import handleMessage from "@/lib/telegram-webhook/handleMessage";
 
 export async function POST(req: NextRequest): Promise<NextResponse>
 {
+	console.log("WEBHOOK HIT");
     const body = await req.json();
+	console.log("Body:", body);
     if (body.message) {
+		console.log("Message:", body.message);
 		await handleMessage(body.message);
 	}
   	return NextResponse.json({ status: "ok" });
