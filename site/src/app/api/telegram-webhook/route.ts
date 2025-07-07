9c6002aec8e54b11f2d83131c29b8bc8091164d0
mikezangus <zangusm@gmail.com>
2025-07-06 23:37:02 -0400

    try and logs

--- site/src/app/api/telegram-webhook/route.ts
+++ site/src/app/api/telegram-webhook/route.ts
@@ -7,9 +7,18 @@ import handleMessage from "@/lib/telegram-webhook/handleMessage";
 
 export async function POST(req: NextRequest): Promise<NextResponse>
 {
-    const body = await req.json();
-    if (body.message) {
-		await handleMessage(body.message);
-	}
-  	return NextResponse.json({ status: "ok" });
+	console.log("webhook | hit")
+	try {
+		const body = await req.json();
+		console.log("webhook | body stored")
+		if (body.message) {
+			console.log("webhook | entering handleMessage")
+			await handleMessage(body.message);
+		}
+		console.log("webhook | returning")
+		return NextResponse.json({ status: "ok" });
+	} catch (err) {
+        console.error("Error from telegram-webook", err);
+        return NextResponse.json({ status: "error handled" });
+    }
 }
