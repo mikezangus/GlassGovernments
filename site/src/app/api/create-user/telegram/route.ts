7b154d82cd25329e9cbd8d9bba8db99b6c9f31d1
mikezangus <zangusm@gmail.com>
2025-07-07 23:32:35 -0400

    return type, no more userContactID

--- site/src/app/api/create-user/telegram/route.ts
+++ site/src/app/api/create-user/telegram/route.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import createUserTelegram from "@/lib/create-user/telegram/createUserTelegram";
 
 
-export async function POST()
+export async function POST(): Promise<NextResponse>
 {
     try {
-        const { userID, userContactID, linkToken } = await createUserTelegram();
-        console.log(`Endpoint | Values returned from createUserTelegram: userID=${userID}, userContactID=${userContactID}, linkToken=${linkToken}`);
-        return NextResponse.json({ userID, userContactID, linkToken });
+        const { userID, linkToken } = await createUserTelegram();
+        console.log(`Endpoint | Values returned from createUserTelegram: userID=${userID}, linkToken=${linkToken}`);
+        return NextResponse.json({ userID, linkToken });
     } catch (err) {
         return NextResponse.json(
             { error: err || "Failed to create Telegram user" },
