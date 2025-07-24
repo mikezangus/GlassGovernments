aa0a6c47360eb8806575253ff5ab9cc1ed3e178a
mikezangus <zangusm@gmail.com>
2025-07-23 23:31:12 -0400

    fetch states on page load

--- site/src/app/bill-tracking/page.tsx
+++ site/src/app/bill-tracking/page.tsx
@@ -1,9 +1,29 @@
 import BillTrackingComponent from "@/components/bill-tracking/BillTracking";
+import styles from "@/styles/bill-tracking/BillTracking.module.css";
 
 
-export default function BillTrackingPage()
+async function fetchStates(): Promise<string[]>
 {
+    const res = await fetch(
+        `https://glassgovernments.com/api/states`,
+        { next: { revalidate: 3600 } }
+    );
+    if (!res.ok) {
+        throw new Error("Failed to fetch states");
+    }
+    const json = await res.json();
+    return json.states as string[];
+}
+
+
+export default async function BillTrackingPage()
+{
+    const states = await fetchStates();
     return (
-        <BillTrackingComponent />
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <BillTrackingComponent states={states}/>
+            </div>
+        </div>
     );
 }
