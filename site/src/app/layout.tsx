7bfc8d0defb6d52abe69be4dfcd7d7a3968f76df
mikezangus <zangusm@gmail.com>
2025-07-23 23:27:41 -0400

    formatting

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -8,7 +8,10 @@ export const metadata = {
 };
 
 
-export default function RootLayout({ children }: { children: React.ReactNode })
+export default function RootLayout(
+    { children }:
+    { children: React.ReactNode }
+)
 {
     return (
         <html lang="en">
