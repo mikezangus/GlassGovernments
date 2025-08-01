057e436a283aaa67c854b3a2949080f0c465cd02
mikezangus <zangusm@gmail.com>
2025-08-01 01:45:24 -0400

    simplified handshake

--- site/src/utils/bill-tracking/db/telegram/createHandshake.ts
+++ site/src/utils/bill-tracking/db/telegram/createHandshake.ts
@@ -1,36 +1,23 @@
-import { ContactMethod, SubmitStatus, WordAndStates } from "@/types";
 import { v4 as uuid } from "uuid";
+import { WordAndStates } from "@/types";
 
 
 export default async function createHandshake(
-    contactMethod: ContactMethod,
-    items: WordAndStates[],
-    setURL: (nextStep: string) => void,
-    setStatus: (status: SubmitStatus) => void
-): Promise<void>
+    items: WordAndStates[]
+): Promise<string>
 {
-    setStatus(SubmitStatus.Loading);
-    try {
-        if (contactMethod !== ContactMethod.Telegram) {
-            throw new Error(`Bad contact method: ${contactMethod}`);
+    const linkToken = uuid();
+    const res = await fetch(
+        "/api/telegram-handshake",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ linkToken, items })
         }
-        const linkToken = uuid();
-        const res = await fetch(
-            "/api/telegram-handshake",
-            {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ linkToken, items })
-            }
-        );
-        if (!res.ok) {
-            const message = await res.text();
-            throw new Error(`Error creating telegram subscription for linkToken=${linkToken}. Status: ${res.status}. Message: ${message}`);
-        }
-        setURL(`https://t.me/glassgovernments_bot?start=${linkToken}`);
-        setStatus(SubmitStatus.Success);
-    } catch (err) {
-        setStatus(SubmitStatus.Fail);
-        throw new Error(`Error: ${err}`)
+    );
+    if (!res.ok) {
+        const message = await res.text();
+        throw new Error(`Error creating telegram subscription for linkToken=${linkToken}. Status: ${res.status}. Message: ${message}`);
     }
+    return `https://t.me/glassgovernments_bot?start=${linkToken}`;
 }
