5305617012fbf7d1890ede48c40929f06da10883
mikezangus <zangusm@gmail.com>
2025-08-01 01:41:17 -0400

    insert to db

--- pipelines/bill-tracking/states/oh/run_oh.py
+++ pipelines/bill-tracking/states/oh/run_oh.py
@@ -1,18 +1,37 @@
+from states.oh.get_bill_count import get_bill_count
+from states.oh.construct_search_url import construct_search_url
+from states.oh.get_bill_urls import get_bill_urls
+from states.oh.extract_metadata_from_bill_url import extract_metadata_from_bill_url
+from states.oh.construct_bill_id import construct_bill_id
+from states.oh.enums import LegislationType
+from insert_to_db import insert_to_db, OnDuplicate
 
-from get_bill_count import get_bill_count
-from construct_search_url import construct_search_url
-from get_bill_urls import get_bill_urls
-from enums import LegislationType
+
+STATE = "OH"
+SESSION = 136
 
 
 def run_oh():
-    bill_count = get_bill_count(136)
+    bill_count = get_bill_count(SESSION)
     search_urls = []
     for i in range(1, bill_count + 1, 1000):
-        search_urls.append(construct_search_url(136, i, 1000, list(LegislationType)))
+        search_urls.append(construct_search_url(SESSION, i, 1000, list(LegislationType)))
     bill_urls = []
     for search_url in search_urls:
         bill_urls.extend(get_bill_urls(search_url))
+    metadata_rows = []
+    for bill_url in bill_urls:
+        state, session, bill_type, bill_num = extract_metadata_from_bill_url(bill_url)
+        bill_id = construct_bill_id(state, session, bill_type, bill_num)
+        metadata_rows.append({
+            "bill_id": bill_id,
+            "state": state,
+            "session": session,
+            "bill_type": bill_type,
+            "bill_num": bill_num,
+            "bill_url": bill_url
+        })
+    insert_to_db("bill_metadata", metadata_rows, OnDuplicate.IGNORE, "bill_id")
 
 
 if __name__ == "__main__":
