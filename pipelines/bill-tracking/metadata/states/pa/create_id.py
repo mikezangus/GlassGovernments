63cf0c9cd1b4d78cb8902f706c359833d7fdadc7
mikezangus <zangusm@gmail.com>
2025-08-06 20:59:05 -0400

    smells less

--- pipelines/bill-tracking/metadata/states/pa/create_id.py
+++ pipelines/bill-tracking/metadata/states/pa/create_id.py
@@ -1,42 +1,28 @@
+from typing import Callable
+
+
+def _extract_field(
+    guid: str,
+    idx: int,
+    increment_max: int,
+    fn_increment_condition: Callable[[str], bool],
+    field_name: str
+) -> tuple[str, int]:
+    start = idx
+    while idx < increment_max and fn_increment_condition(guid[idx]):
+        idx += 1
+    if idx == start:
+        raise ValueError(f"Failed to extract {field_name} from guid={guid}")
+    return guid[start:idx], idx
+
+
 def create_id(guid: str) -> str:
+    idx = 0
     guid_len = len(guid)
-    guid_idx = 0    
-
-    session = ""
-    while guid_idx < 4 and guid[guid_idx].isdigit():
-        session += guid[guid_idx]
-        guid_idx += 1
-    if len(session) == 0:
-        raise ValueError
-
-    special_session = ""
-    while guid_idx < guid_len and guid[guid_idx].isdigit():
-        special_session += guid[guid_idx]
-        guid_idx += 1
-    if len(special_session) == 0:
-        raise ValueError
-
-    type = ""
-    while guid_idx < guid_len and guid[guid_idx].isalpha():
-        type += guid[guid_idx]
-        guid_idx += 1
-    if len(type) == 0:
-        raise ValueError
-
-    bill_num = ""
-    while guid_idx < guid_len and guid[guid_idx].isdigit():
-        bill_num += guid[guid_idx]
-        guid_idx += 1
-    if len(bill_num) == 0:
-        raise ValueError
-
-    print_num = ""
-    while guid_idx < guid_len and guid[guid_idx].isalpha():
-        guid_idx += 1
-    while guid_idx < guid_len and guid[guid_idx].isdigit():
-        print_num += guid[guid_idx]
-        guid_idx += 1
-    if len(print_num) == 0:
-        raise ValueError
-
+    session, idx = _extract_field(guid, idx, 4, str.isdigit, "session")
+    special_session, idx = _extract_field(guid, idx, guid_len, str.isdigit, "special_session")
+    type, idx = _extract_field(guid, idx, guid_len, str.isalpha, "type")
+    bill_num, idx = _extract_field(guid, idx, guid_len, str.isdigit, "bill_num")
+    _, idx = _extract_field(guid, idx, guid_len, str.isalpha, "skipping 'PN'")
+    print_num, _ = _extract_field(guid, idx, guid_len, str.isdigit, "print_num")
     return f"PA_{session}_{special_session}_{type}_{bill_num}_{print_num}"
