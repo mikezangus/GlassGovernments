32e3b4db9975660f32d3f53149ae9736d271f568
mikezangus <zangusm@gmail.com>
2025-08-15 23:34:53 -0400

    extract raw actions

---
+++ pipelines/bill-tracking/actions/states/oh/extract_raw_actions.py
@@ -0,0 +1,42 @@
+from lxml import html as lxml
+from lxml.html import HtmlElement
+from shared.utils.normalize_null import normalize_null
+
+
+def _get_chamber_node(doc: HtmlElement, header_text: str) -> HtmlElement | None:
+    nodes = doc.xpath(f"//div[@class='group-header'][normalize-space()='{header_text}']/following-sibling::div[1]")
+    return nodes[0] if nodes else None
+
+
+def _extract_fields(section_div: HtmlElement) -> dict[str, str]:
+    input_fields = section_div.xpath(
+        ".//div[contains(@class,'input-group')]"
+        "//div[contains(@class,'input-group-text') and contains(@class,'w-100')]"
+    )
+    output_fields: dict[str, str] = {}
+    for field in input_fields:
+        label = normalize_null(' '.join(field.itertext()))
+        contents_div = field.xpath("parent::div/following-sibling::div[contains(@class,'form-control')][1]")
+        if not contents_div:
+            contents_div = field.xpath("parent::div/following-sibling::div[contains(@class,'form-control') and contains(@class,'textarea')][1]")
+        contents = normalize_null(' '.join(contents_div[0].itertext())) \
+            if contents_div \
+            else None
+        if contents:
+            output_fields[label] = contents
+    return output_fields
+
+
+def extract_raw_actions(html: str) -> list[dict[str, str]]:
+    doc: HtmlElement = lxml.fromstring(html)
+    nodes = [
+        ("lower", _get_chamber_node(doc, "House")),
+        ("upper", _get_chamber_node(doc, "Senate")),
+        ("executive", _get_chamber_node(doc, "Governor")),
+        ("veto", _get_chamber_node(doc, "Veto")),
+        ("enacted", _get_chamber_node(doc, "Effective Date"))
+    ]
+    raw_actions = {}
+    for chamber, node in nodes:
+        raw_actions[chamber] = _extract_fields(node) if node is not None else {}
+    return raw_actions
